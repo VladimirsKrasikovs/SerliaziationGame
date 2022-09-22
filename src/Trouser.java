f3bf3d6fbefad71659163dfe23bd63739f61fbfa
VladimirsKrasikovs <108757262+VladimirsKrasikovs@users.noreply.github.com>
2022-09-22 23:16:06 +0300

    Initial commit

---
+++ src/Trouser.java
@@ -0,0 +1,14 @@
+
+import java.io.Serializable;
+
+
+public class Trouser extends Armour implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    /* Constructors */
+    public Trouser(String name, int durability, int defenseRating) {
+        super(name, durability, defenseRating);
+    }
+}
+
